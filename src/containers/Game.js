b38285e52f05e3af5186fa43da5a467a1863b21b
Nicholas Garcia <nickgarcia0523@gmail.com>
2025-10-29 22:12:46 -0700

    Update Game.js

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import App from '../App';
 import carsGame from '../utils/carsGame.js';
-
 const theCars = new carsGame();
-
 class Game extends Component {
   constructor(props) {
     super(props);
     theCars.setRedrawCallback(() => this.forceUpdate());
   }
-
   render() {
     return <App cars={theCars} />;
   }
 }
-
 export default Game;
